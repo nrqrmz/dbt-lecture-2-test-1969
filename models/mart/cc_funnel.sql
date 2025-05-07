2aec3c1be33e45890b594352ca1acdda894f8bdf
Enrique Ramirez Ramos <nrqrmz@gmail.com>
2025-05-07 05:42:18 +0000

    add mart model

---
+++ models/mart/cc_funnel.sql
@@ -0,0 +1,18 @@
+SELECT
+company
+, sector
+, priority
+, date_lead
+, date_opportunity
+, date_customer
+, date_lost
+, deal_stage
+, lead2customer
+, lead2opportunity
+, opportunity2customer
+, lead2customer_time
+, lead2opportunity_time
+, opportunity2customer_time
+FROM {{ref("int_funnel")}}
+left join {{ref("stg_raw__priority")}}
+using(company)
